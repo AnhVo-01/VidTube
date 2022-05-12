022492a51602c4457a7cbe8f980f4992b70725c8
Avin-Vo <anhvo.1305@gmail.com>
2022-05-12 17:01:35 +0700

    simple slide show

--- js/slide-show.js
+++ js/slide-show.js
@@ -1,14 +1,20 @@
 let slideIndex = 0;
 showSlides();
 
-function showSlides() {
-  let i;
-  let slides = document.getElementsByClassName("mySlides");
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
-  }
-  slideIndex++;
-  if (slideIndex > slides.length) {slideIndex = 1}
-  slides[slideIndex-1].style.display = "block";
-  setTimeout(showSlides, 5000); // Change image every 5 seconds
+function showSlides(){
+    var slides = document.getElementsByClassName("sl-item"),
+        slideBox = document.querySelector(".side-box");
+    for (let i = 0; i < slides.length; i++) {
+        if(slides[i].classList.contains("slide") == false){
+            slides[i].classList.toggle("slide");
+            // slideBox.removeChild(slides[i]);
+        }
+    }
+    slideIndex++;
+    if (slideIndex > slides.length) {
+        slideIndex = 1
+    }
+    slides[slideIndex-1].classList.remove("slide");
+    // slideBox.appendChild(slides[slideIndex-1]);
+    setTimeout(showSlides, 2000); // Change image every 2 seconds
 }
